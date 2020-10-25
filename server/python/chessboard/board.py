03e05a5c77f938fe418c64d78bd589bc2e99fb71
Robby <bergersr97@gmail.com>
2020-10-25 18:05:24 -0400

    Remove divide / 2 from the sum

--- server/python/chessboard/board.py
+++ server/python/chessboard/board.py
@@ -35,7 +35,7 @@ class Board:
         for queen in self.queens:
             captures += self.board[queen[0]][queen[1]].getSum()
 
-        return int(captures / 2)
+        return captures
 
 
     def getQueens(self):
